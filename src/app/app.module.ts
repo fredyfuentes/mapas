2b08e07fd306760cc8d7c2862dc94fe416d87dfb
Fredy Fuentes <fredy.fuentesb@gmail.com>
2019-09-17 21:56:30 -0500

    agregar marcadores al mapa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+import { MapaComponent } from './components/mapa/mapa.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBH2darVLJnTy2kJnn5gV2HDBzSCynuZP8'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
